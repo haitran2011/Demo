d82960558f60817146e01669a10e4ddbcfa21ac0
hdhaitran <haitran@admins-MacBook-Pro.local>
2016-09-06 10:34:44 +0700

    add fun update

--- Demo/ViewController.swift
+++ Demo/ViewController.swift
@@ -46,7 +46,9 @@ class ViewController: UIViewController {
     func delete()  {
     
     }
-
+    func update()  {
+        
+    }
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
