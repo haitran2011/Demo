1dc1d1215164be4c03af850d9a8fd4c41443d2c2
haitran <haitran@admins-MacBook-Pro.local>
2016-09-06 09:26:48 +0700

    init project

--- Demo/ViewController.swift
+++ Demo/ViewController.swift
@@ -7,12 +7,37 @@
 //
 
 import UIKit
+import CoreData
 
 class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        var count1 = 0
+        
+    
+        var count2 = 0
+        
+        let  queue = dispatch_queue_create("queue", DISPATCH_QUEUE_SERIAL)
+        dispatch_async(queue) {
+            for _ in 1...1000000000
+            {
+                count1 += 1
+                
+            }
+        }
+        
+        dispatch_async(queue) {
+            for _ in 1...10000
+            {
+                count2 += 1
+                
+            }
+        }
+        print(count1)
+        print(count2)
     }
 
     override func didReceiveMemoryWarning() {
