b620946679ace452efb2be0d4d0dd78fc326c0c8
hdhaitran <haitran@admins-MacBook-Pro.local>
2016-09-06 11:24:12 +0700

    no message

--- Demo/ViewController.swift
+++ Demo/ViewController.swift
@@ -53,6 +53,10 @@ class ViewController: UIViewController {
     func update4()  {
         
     }
+    
+    func haitran()  {
+        
+    }
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
